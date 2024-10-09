c2abd6e1eb93010dcd2f068c57611eba0e27a5ab
Michal Skop <michal.skop@kohovolit.eu>
2024-10-09 02:45:24 +0200

    index

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
