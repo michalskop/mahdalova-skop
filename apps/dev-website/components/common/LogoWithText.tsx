b92c17e1aacbec58c9d31bfbc63727edfdfe2844
Michal Skop <michal.skop@kohovolit.eu>
2024-11-25 01:20:57 +0100

    logo improved, close #17

--- apps/dev-website/components/common/LogoWithText.tsx
+++ apps/dev-website/components/common/LogoWithText.tsx
@@ -2,12 +2,13 @@ import { Group, Text } from '@mantine/core';
 import Link from 'next/link';
 
 const Logo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 500 500">
+  // center of the svg: 250, 250; last point is slightly adjust manually to make the circle looked closed even in small sizes
+  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="60 60 330 330">
     <title>Logo</title>
-    <path fill="none" stroke="#ffcf02" strokeWidth="48" strokeLinecap="round" d="M 234.7876613810083 348.8361510467761 A 100 100 0 0 0 336.60254037844385 300.00000000000006"/>
-    <path fill="none" stroke="#f76800" strokeWidth="48" strokeLinecap="round" d="M 163.39745962155612 200.0 A 100 100 0 0 0 234.7876613810083 348.8361510467761"/>
-    <path fill="none" stroke="#de1743" strokeWidth="48" strokeLinecap="round" d="M 336.6025403784439 300 A 100 100 0 0 0 163.39745962155612 200"/>
-    <path fill="none" stroke="#ffcf02" strokeWidth="48" strokeLinecap="round" d="M 335.7167300702112 301.50380749100543 A 100 100 0 0 0 336.60254037844385 300.00000000000"/>
+    <path fill="none" stroke="#ffdc33" stroke-width="76" stroke-linecap="round" d="M 234.7876613810083 348.8361510467761 A 100 100 0 0 0 336.60254037844385 300"/>
+    <path fill="none" stroke="#f76800" stroke-width="76" stroke-linecap="round" d="M 163.39745962155612 200.0 A 100 100 0 0 0 234.7876613810083 348.8361510467761"/>
+    <path fill="none" stroke="#de1743" stroke-width="76" stroke-linecap="round" d="M 336.6025403784439 300 A 100 100 0 0 0 163.39745962155612 200"/>
+    <path fill="none" stroke="#ffdc33" strokeWidth="78" strokeLinecap="round" d="M 325.47 315.61 A 100 100 0 0 0 341 291"/>
   </svg>
 );
 
