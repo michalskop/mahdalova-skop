eb8763d674fea9ef9ffa748afbe63d146f7def90
Michal Skop <michal.skop@kohovolit.eu>
2024-12-06 23:14:14 +0100

    adding newsletter to analyses and context

--- apps/dev-website/app/kontext/page.tsx
+++ apps/dev-website/app/kontext/page.tsx
@@ -2,6 +2,7 @@
 import { getArticles } from '@/components/common/getArticles';
 import { ArticlesSection } from '@/components/common/ArticlesSection';
 import { Container } from '@mantine/core';
+import SubscribeNewsletter from '@/components/common/SubscribeNewsletter';
 
 export default async function ArticlesPage() {
   const articles = await getArticles(100, "kontext");
@@ -21,6 +22,8 @@ export default async function ArticlesPage() {
         articles={articles}
         themeColor="brandOrange.4"
       />
+      
+      <SubscribeNewsletter actionUrl='https://mahdalovaskop.ecomailapp.cz/public/subscribe/1/43c2cd496486bcc27217c3e790fb4088'/>
     </Container>
   );
 }
