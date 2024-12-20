cc4c6f5e5eb2c833dd7f15116c477ce7b5e33c52
Michal Skop <michal.skop@kohovolit.eu>
2024-12-20 03:49:50 +0100

    switching web - dev

--- apps/dev-website/next.config.mjs
+++ apps/dev-website/next.config.mjs
@@ -1,24 +1,16 @@
-// apps/dev-site/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'export',
   experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-    mdxRs: true,
+    esmExternals: false
   },
-  images: {
-    unoptimized: true, // Disable image optimization since Cloudflare Pages handles this
-  },
-  webpack: (config, { webpack }) => {
-    // Optimize bundle size
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-    }
-    
-    return config
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/index.html',
+      },
+    ];
   },
 };
 
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,16 +1,24 @@
+// apps/web/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
   experimental: {
-    esmExternals: false
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+    mdxRs: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/index.html',
-      },
-    ];
+  images: {
+    unoptimized: true, // Disable image optimization since Cloudflare Pages handles this
+  },
+  webpack: (config, { webpack }) => {
+    // Optimize bundle size
+    config.optimization = {
+      ...config.optimization,
+      minimize: true,
+    }
+    
+    return config
   },
 };
 
