14763f27edabfd9380f204642d4143bc4c25487a
Michal Skop <michal.skop@kohovolit.eu>
2025-10-12 23:38:12 +0200

    logo fix

--- apps/web/components/common/Logo.tsx
+++ apps/web/components/common/Logo.tsx
@@ -5,10 +5,10 @@ const Logo = () => (
   <Link href="/" className="hover:opacity-80 transition-opacity">
     <Group gap="xs">
       <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 500 500">
-        <path title="yellow" fill="none" stroke="yellow" strokeWidth="48" strokeLinecap="round" d="M 234.7876613810083 348.8361510467761 A 100 100 0 0 0 336.60254037844385 300.00000000000006"/>
-        <path title="orange" fill="none" stroke="orange" strokeWidth="48" strokeLinecap="round" d="M 163.39745962155612 200.0 A 100 100 0 0 0 234.7876613810083 348.8361510467761"/>
-        <path title="red" fill="none" stroke="#f20000" strokeWidth="48" strokeLinecap="round" d="M 336.6025403784439 300 A 100 100 0 0 0 163.39745962155612 200"/>
-        <path title="yellow fix" fill="none" stroke="yellow" strokeWidth="48" strokeLinecap="round" d="M 335.7167300702112 301.50380749100543 A 100 100 0 0 0 336.60254037844385 300.00000000000"/>
+        <path fill="none" stroke="yellow" strokeWidth="48" strokeLinecap="round" d="M 234.7876613810083 348.8361510467761 A 100 100 0 0 0 336.60254037844385 300.00000000000006"/>
+        <path fill="none" stroke="orange" strokeWidth="48" strokeLinecap="round" d="M 163.39745962155612 200.0 A 100 100 0 0 0 234.7876613810083 348.8361510467761"/>
+        <path fill="none" stroke="#f20000" strokeWidth="48" strokeLinecap="round" d="M 336.6025403784439 300 A 100 100 0 0 0 163.39745962155612 200"/>
+        <path fill="none" stroke="yellow" strokeWidth="48" strokeLinecap="round" d="M 335.7167300702112 301.50380749100543 A 100 100 0 0 0 336.60254037844385 300.00000000000"/>
       </svg>
     </Group>
   </Link>
