45a55c5b077bafbc14ece55cfcfb36408fe89937
Michal Skop <michal.skop@kohovolit.eu>
2026-04-04 00:56:11 +0200

    logo fix -30

--- apps/web/components/common/LogoWithText.tsx
+++ apps/web/components/common/LogoWithText.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const Logo = () => (
   // center of the svg: 250, 250; last point is slightly adjust manually to make the circle looked closed even in small sizes
-  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="60 60 330 330" transform="rotate(345)">
+  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="60 60 330 330">
     <defs>
       <linearGradient id="Gradient" x1="1" x2="0.25" y1="0.5" y2="1">
         <stop offset="0%" stop-color="#ffdc33" stop-opacity="0"  />
@@ -14,10 +14,12 @@ const Logo = () => (
         <stop offset="100%" stop-color="#ffdc33" stop-opacity="1" />
       </linearGradient>
     </defs>
-    <path fill="none" stroke="#ffdc33" strokeWidth="76" strokeLinecap="round" d="M 250 350 A 100 100 0 0 0 336.60254037844385 300"/>
-    <path fill="none" stroke="#de1743" strokeWidth="76" strokeLinecap="round" d="M 336.6025403784439 300 A 100 100 0 0 0 163.39745962155612 200"/>
-    <path fill="none" stroke="#f76800" strokeWidth="76" strokeLinecap="round" d="M 163.39745962155612 200 A 100 100 0 0 0 250 350"/>
-    <path fill="none" stroke="url(#Gradient)" strokeWidth="76" strokeLinecap="round" d="M 250 350 A 100 100 0 0 0 336.60254037844385 300"/>
+    <g transform="rotate(-30 250 250)">
+      <path fill="none" stroke="#ffdc33" strokeWidth="76" strokeLinecap="round" d="M 250 350 A 100 100 0 0 0 336.60254037844385 300"/>
+      <path fill="none" stroke="#f76800" strokeWidth="76" strokeLinecap="round" d="M 336.6025403784439 300 A 100 100 0 0 0 250 150"/>
+      <path fill="none" stroke="#de1743" strokeWidth="76" strokeLinecap="round" d="M 250 150 A 100 100 0 0 0 250 350"/>
+      <path fill="none" stroke="url(#Gradient)" strokeWidth="76" strokeLinecap="round" d="M 250 350 A 100 100 0 0 0 336.60254037844385 300"/>
+    </g>
   </svg>
 );
 
